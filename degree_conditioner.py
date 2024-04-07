58e0fe35897c29e74e15c5877d96a9a31b6cff5b
EyadGamer1757 <166323143+EyadGamer1757@users.noreply.github.com>
2024-04-08 00:46:13 +0200

    Add files via upload

---
+++ degree_conditioner.py
@@ -0,0 +1,18 @@
+degree=int(input(""))
+print(degree)
+if degree>=50 and degree < 70:
+    print("Acceptable")
+elif degree<50 and degree < 50:
+    print("Loser Loser Loser Loser Loser")
+elif degree>=70 and degree < 80:
+    print("You're good")
+elif degree>=80 and degree < 90:
+    print("Excelent")
+elif degree>=90 and degree<=100:
+    print("You're Ineshtine")
+    print("One Piece will be proud of you 😊")
+    print("Winner winner chicken dinner")
+    print("Winner winner chicken dinner")
+    print("Winner winner chicken dinner")
+elif degree > 100:
+    print("Don't lie")
